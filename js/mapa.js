9dd8a09d420759d55ab259ea146943e97d1d20a7
Mary Luz <mary.quispe1@unmsm.edu.pe>
2021-09-04 21:27:15 -0500

    Correccion

---
+++ js/mapa.js
@@ -0,0 +1,11 @@
+function iniciarMap(){
+    var coord = {lat:-12.084268 ,lng: -77.08291};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
